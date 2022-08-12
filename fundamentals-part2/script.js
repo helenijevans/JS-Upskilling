b16977a130bd99561fffae6811355e370de068e3
Helen Evans <helenijevans@gmail.com>
2022-08-12 16:55:04 +0100

    done 2.2

--- fundamentals-part2/script.js
+++ fundamentals-part2/script.js
@@ -1,45 +1,63 @@
 'use strict';
 
-function describeCountry(country, population, capitalCity) {
-    return `${country} has ${population} people and its capital city is ${capitalCity}`
-}
+// function describeCountry(country, population, capitalCity) {
+//     return `${country} has ${population} people and its capital city is ${capitalCity}`
+// }
 
 
-describeCountry("Finland", "6 million", "Helsinki")
-const malaysia = describeCountry("Malaysia", "33 million", "Kuala Lumpur")
-const norway = describeCountry("Norway", "12 million", "Oslo")
-const brazil = describeCountry("Brazil", "50 million", "Brasilia")
-console.log(`
-${malaysia}
-${norway}
-${brazil}
-`)
+// describeCountry("Finland", "6 million", "Helsinki")
+// const malaysia = describeCountry("Malaysia", "33 million", "Kuala Lumpur")
+// const norway = describeCountry("Norway", "12 million", "Oslo")
+// const brazil = describeCountry("Brazil", "50 million", "Brasilia")
+// console.log(`
+// ${malaysia}
+// ${norway}
+// ${brazil}
+// `)
 
 function percentageOfWorld1(population) {
     return (population / 7900000000) * 100
 }
 
-console.log(percentageOfWorld1(33000000))
-console.log(percentageOfWorld1(57000000))
-console.log(percentageOfWorld1(100000000))
+// console.log(percentageOfWorld1(33000000))
+// console.log(percentageOfWorld1(57000000))
+// console.log(percentageOfWorld1(100000000))
 
-const percentageOfWorld2 = function (population) {
-    return (population / 7900000000) * 100
+// const percentageOfWorld2 = function (population) {
+//     return (population / 7900000000) * 100
+// }
+
+// console.log(percentageOfWorld2(33000000))
+// console.log(percentageOfWorld2(57000000))
+// console.log(percentageOfWorld2(100000000))
+
+// const percentageOfWorld3 = population => (population / 7900000000) * 100
+
+// console.log(percentageOfWorld3(33000000))
+// console.log(percentageOfWorld3(57000000))
+// console.log(percentageOfWorld3(100000000))
+
+// function describePopulation(country, population) {
+//     return `${country} has ${population} people, which is about ${percentageOfWorld1(population)}% of the world`
+// }
+// console.log(describePopulation('Malaysia', 33000000))
+// console.log(describePopulation('Norway', 57000000))
+// console.log(describePopulation('Brazil', 100000000))
+
+// const populations = [33000000, 57000000, 100000000, 22000000]
+// console.log(populations.length === 4)
+// const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])]
+
+// console.log(percentages);
+
+const neighbours = ["Thailand", "Indonesia", "Singapore"]
+neighbours.push("Utopia")
+console.log(neighbours)
+neighbours.pop()
+console.log(neighbours)
+if (!neighbours.includes("Germany")) {
+    console.log('Probably not a central European country :D')
 }
-
-console.log(percentageOfWorld2(33000000))
-console.log(percentageOfWorld2(57000000))
-console.log(percentageOfWorld2(100000000))
-
-const percentageOfWorld3 = population => (population / 7900000000) * 100
-
-console.log(percentageOfWorld3(33000000))
-console.log(percentageOfWorld3(57000000))
-console.log(percentageOfWorld3(100000000))
-
-function describePopulation(country, population) {
-    return `${country} has ${population} people, which is about ${percentageOfWorld1(population)}% of the world`
-}
-console.log(describePopulation('Malaysia', 33000000))
-console.log(describePopulation('Norway', 57000000))
-console.log(describePopulation('Brazil', 100000000))
+const index = neighbours.indexOf("Indonesia")
+neighbours[index] = "Malaysia's Inferior ;)"
+console.log(neighbours)
